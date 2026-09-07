493f2e1d6c3ecaa70cafdb8648fa21e7064d7963
luisflr <lfloresr@ulasalle.edu.pe>
2026-09-06 22:56:48 -0500

    remove console logs

--- auth.ts
+++ auth.ts
@@ -37,7 +37,6 @@ export const { auth, signIn, signOut } = NextAuth({
           if (passwordsMatch) return user;
         }
 
-        console.log("Invalid credentials");
         return null;
       },
     }),
